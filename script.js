318f2ccbd84a3a91dd4193bc9a5291a7aef7a3d2
Abdurahim102 <abundance0121@gmail.com>
2024-02-24 10:04:38 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+// function first(){
+//     setTimeout(()=>{
+//      console.log("ish boshlandi")
+       
+//     },1000)
+    
+// } 
+
+// function two(){
+//     setTimeout(()=>{
+//     console.log("1")
+//     }, 2000)
+    
+// }
+// function lol(){
+//     setTimeout(()=>{
+//     console.log("2")
+//     }, 3000)
+    
+// }
+// function gr(){
+//     setTimeout(()=>{
+//     console.log("3")
+//     }, 4000)
+    
+// }
+// function rur(){
+//     setTimeout(()=>{
+//     console.log("4")
+//     }, 5000)
+    
+// }
+// function ol(){
+//     setTimeout(()=>{
+//     console.log("5")
+//     }, 6000)
+    
+// }
+// function secont(){
+//     setTimeout(()=>{
+//     console.log("ish tugadi")
+//     }, 7000)
+    
+// }
+// first()
+// secont()
+// two()
+// lol()
+// gr()
+// rur()
+// ol()
+
+
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { c: 3, d: 4 };
+
+// const obj = Object.assign(obj1,obj2);
+
+
+// console.log(obj);
+// //{ a: 1, b: 2, c: 3, d: 4 }    
+
+
+// const num1 = [1,2,3]
+// const num2 = [4,5,6]
+
+//  // birnchi num1 ichiga ikkinchi num2 yig'ilsin
+// const obj = num1.concat(num2)
+// console.log(obj);
+// // [1,2,3,4,5,6]
+
+
+// const user = {name: "Abdulloh", job: "Desnger"}
+
+// const admin = {dev: true}
+
+// const userandadmin = Object.assign(admin,user)
+// // var string2= JSON.stringify(admin); 
+// // var string = JSON.stringify(user); 
+// console.log(userandadmin);
+
+
+
+ // admin ichiga user qushilib ketsin 
+
+// {dev: true, name: "Abdulloh", job: "Desnger"}
